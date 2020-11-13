5c4f6ba57e57db2e881a6ee509f25d70b7fd5bb7
selle-dev <74355536+selle-dev@users.noreply.github.com>
2020-11-13 04:10:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,51 @@
-const version = 'v1';
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(version).then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/shows.html',
-        '/about.html',
-        '/style-v1.css',
-        '/script.js',
-        '/notfound.txt'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).then(function(response) {
-    // caches.match() always resolves
-    // but in case of success response will have value
-    if (response !== undefined) {
-      return response;
-    } else {
-      return fetch(event.request).then(function (response) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        let responseClone = response.clone();
-
-        caches.open(version).then(function (cache) {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      }).catch(function () {
-        return caches.match('/notfound.txt');
+self.addEventListener("install", function(event) {
+    event.waitUntil(preLoad());
+  });
+  
+  var preLoad = function(){
+    console.log("Installing web app");
+    return caches.open("offline").then(function(cache) {
+      console.log("caching index and important routes");
+      return cache.addAll(["/"]);
+    });
+  };
+  
+  self.addEventListener("fetch", function(event) {
+    event.respondWith(checkResponse(event.request).catch(function() {
+      return returnFromCache(event.request);
+    }));
+    event.waitUntil(addToCache(event.request));
+  });
+  
+  var checkResponse = function(request){
+    return new Promise(function(fulfill, reject) {
+      fetch(request).then(function(response){
+        if(response.status !== 404) {
+          fulfill(response);
+        } else {
+          reject();
+        }
+      }, reject);
+    });
+  };
+  
+  var addToCache = function(request){
+    return caches.open("offline").then(function (cache) {
+      return fetch(request).then(function (response) {
+        console.log(response.url + " was cached");
+        return cache.put(request, response);
       });
-    }
-  }));
-});
+    });
+  };
+  
+  var returnFromCache = function(request){
+    return caches.open("offline").then(function (cache) {
+      return cache.match(request).then(function (matching) {
+       if(!matching || matching.status == 404) {
+         return cache.match("offline.html");
+       } else {
+         return matching;
+       }
+      });
+    });
+  };
